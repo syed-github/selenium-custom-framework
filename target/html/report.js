bb06e690d80b82c69e9522c8f52aecff770c9074
Syed <syed.qae@gmail.com>
2017-03-27 20:55:51 -0400

    commiting report

---
+++ target/html/report.js
@@ -0,0 +1,339 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("AppLoginTest.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "#Author: syed.qae@gmail.com"
+    },
+    {
+      "line": 2,
+      "value": "#Keywords Summary :"
+    },
+    {
+      "line": 3,
+      "value": "#Feature: List of scenarios."
+    },
+    {
+      "line": 4,
+      "value": "#Scenario: Business rule through list of steps with arguments."
+    },
+    {
+      "line": 5,
+      "value": "#Given: Some precondition step"
+    },
+    {
+      "line": 6,
+      "value": "#When: Some key actions"
+    },
+    {
+      "line": 7,
+      "value": "#Then: To observe outcomes or validation"
+    },
+    {
+      "line": 8,
+      "value": "#And,But: To enumerate more Given,When,Then steps"
+    },
+    {
+      "line": 9,
+      "value": "#Scenario Outline: List of steps for data-driven as an Examples and \u003cplaceholder\u003e"
+    },
+    {
+      "line": 10,
+      "value": "#Examples: Container for s table"
+    },
+    {
+      "line": 11,
+      "value": "#Background: List of steps run before each of the scenarios"
+    },
+    {
+      "line": 12,
+      "value": "#\"\"\" (Doc Strings)"
+    },
+    {
+      "line": 13,
+      "value": "#| (Data Tables)"
+    },
+    {
+      "line": 14,
+      "value": "#@ (Tags/Labels):To group Scenarios"
+    },
+    {
+      "line": 15,
+      "value": "#\u003c\u003e (placeholder)"
+    },
+    {
+      "line": 16,
+      "value": "#\"\""
+    },
+    {
+      "line": 17,
+      "value": "## (Comments)"
+    },
+    {
+      "line": 19,
+      "value": "#Sample Feature Definition Template"
+    }
+  ],
+  "line": 21,
+  "name": "Title of your feature",
+  "description": "I want to use this template for my feature file",
+  "id": "title-of-your-feature",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 20,
+      "name": "@tag"
+    }
+  ]
+});
+formatter.scenario({
+  "line": 25,
+  "name": "Title of your scenario",
+  "description": "",
+  "id": "title-of-your-feature;title-of-your-scenario",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 24,
+      "name": "@tag1"
+    }
+  ]
+});
+formatter.step({
+  "line": 26,
+  "name": "I want to write a step with precondition",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 27,
+  "name": "some other precondition",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I complete action",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 29,
+  "name": "some other action",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "yet another action",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "I validate the outcomes",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 32,
+  "name": "check more outcomes",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenarioOutline({
+  "line": 35,
+  "name": "Title of your scenario outline",
+  "description": "",
+  "id": "title-of-your-feature;title-of-your-scenario-outline",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 34,
+      "name": "@tag2"
+    }
+  ]
+});
+formatter.step({
+  "line": 36,
+  "name": "I want to write a step with \u003cname\u003e",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 37,
+  "name": "I check for the \u003cvalue\u003e in step",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 38,
+  "name": "I verify the \u003cstatus\u003e in step",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 40,
+  "name": "",
+  "description": "",
+  "id": "title-of-your-feature;title-of-your-scenario-outline;",
+  "rows": [
+    {
+      "cells": [
+        "name",
+        "value",
+        "status"
+      ],
+      "line": 41,
+      "id": "title-of-your-feature;title-of-your-scenario-outline;;1"
+    },
+    {
+      "cells": [
+        "name1",
+        "5",
+        "success"
+      ],
+      "line": 42,
+      "id": "title-of-your-feature;title-of-your-scenario-outline;;2"
+    },
+    {
+      "cells": [
+        "name2",
+        "7",
+        "Fail"
+      ],
+      "line": 43,
+      "id": "title-of-your-feature;title-of-your-scenario-outline;;3"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 42,
+  "name": "Title of your scenario outline",
+  "description": "",
+  "id": "title-of-your-feature;title-of-your-scenario-outline;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 20,
+      "name": "@tag"
+    },
+    {
+      "line": 34,
+      "name": "@tag2"
+    }
+  ]
+});
+formatter.step({
+  "line": 36,
+  "name": "I want to write a step with name1",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 37,
+  "name": "I check for the 5 in step",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 38,
+  "name": "I verify the success in step",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "line": 43,
+  "name": "Title of your scenario outline",
+  "description": "",
+  "id": "title-of-your-feature;title-of-your-scenario-outline;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 20,
+      "name": "@tag"
+    },
+    {
+      "line": 34,
+      "name": "@tag2"
+    }
+  ]
+});
+formatter.step({
+  "line": 36,
+  "name": "I want to write a step with name2",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 37,
+  "name": "I check for the 7 in step",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 38,
+  "name": "I verify the Fail in step",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
